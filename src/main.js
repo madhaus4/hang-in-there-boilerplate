092e3646f07d6eb16e38340091a790c786504762
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-05-19 21:21:28 -0600

    Complete main to form transition

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@
 var posterImg = document.querySelector('.poster-img');
 var posterTitle = document.querySelector('.poster-title');
 var posterQuote = document.querySelector('.poster-quote');
+var posterForm = document.querySelector('.poster-form');
+var posterMain = document.querySelector('.main-poster');
+
+
 
 // BUTTONS
-var showRandomBtn= document.querySelector('.show-random');
+var showRandomBtn = document.querySelector('.show-random');
+var showFormBtn = document.querySelector('.show-form');
 
 var savedPosters = [];
 var currentPoster;
@@ -12,6 +17,7 @@ var currentPoster;
 // EVENTLISTENERS
 document.addEventListener('DOMContentLoaded', randomizePoster);
 showRandomBtn.addEventListener('click', randomizePoster);
+showFormBtn.addEventListener('click', showFormView);
 
 // EVENTHANDLERS
 function getRandomIndex(array) {
@@ -22,4 +28,9 @@ function randomizePoster() {
   posterImg.src = images[getRandomIndex(images)];
   posterTitle.innerText = titles[getRandomIndex(titles)];
   posterQuote.innerText = quotes[getRandomIndex(quotes)];
+}
+
+function showFormView() {
+  posterForm.classList.remove('hidden');
+  posterMain.classList.add('hidden');
 }
